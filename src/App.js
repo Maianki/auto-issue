6917d41d40233cdebb69a3798f8d68ef0a9a2b14
vrinda-mahajan <vrindamahajan96@gmail.com>
2022-05-07 19:06:26 +0530

    Feat: Added popup and modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import capture from "./assets/capture.png"
+import { ModalContainer } from "./modal";
 
 function App() {
+  const [dropdown,setDropdown] = useState(false);
   return (
-    <div className='App'>
-      <h1>Landing page</h1>
+    <div>
+      <div className="popup-container">
+        <h3 className="extension-name text-bold-weight">Create Auto issues</h3>
+        <div className="issue-title text-xlg">Spelling Mistake</div>
+        <img className="issue-img" src={capture} alt="issue-img"/>
+        <div className="issue-desc text-lg">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..</div>
+        <div className="issue-footer flex-r">
+          <button className="btn btn-primary text-md">Create Issue</button>
+          <button onClick={()=>{setDropdown(prev=>!prev)}} className="m-left-auto btn-icon1 btn footer-icon">
+                      <i className="fa-solid fa-ellipsis-vertical"></i>
+          </button>
+        </div>
+
+        {dropdown && <div onClick={()=>{setDropdown(prev=>!prev)}} className="list-non-bullet dropdown-section">
+              <li className="dropdown-item">
+                  <i className="fa-solid fa-pen"></i>
+                  <span>Edit</span>
+              </li>
+              <li className="dropdown-item">
+                  <i className="fa-solid fa-trash-can"></i>
+                  <span>Delete</span>
+              </li>
+          </div>} 
+      </div>
+      <ModalContainer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider,extendTheme } from '@chakra-ui/react'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const customTheme = extendTheme({
+  colors: {
+    brand: {
+      // main: "#222831",
+      100: "#222831",
+      200: "#393E46",
+      300: "#00ADB5",
+      400: "#EEEEEE",
+    },
+  },
+})
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
