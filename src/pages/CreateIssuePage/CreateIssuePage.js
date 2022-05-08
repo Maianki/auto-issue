3ffda49a4ac0b72480daa9d50b6ce757656a2041
Ankit Kumain <kumankit10@gmail.com>
2022-05-08 16:23:05 +0530

    style: add container and min-width

--- src/pages/CreateIssuePage/CreateIssuePage.js
+++ src/pages/CreateIssuePage/CreateIssuePage.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Container, Input, Text, Textarea } from "@chakra-ui/react";
+import { Input, Text, Textarea } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export function CreateIssuePage() {
   return (
-    <Container maxW='4xl'>
+    <div className='container-create-issue'>
       <h1 className='create-issue-heading text-bold-weight'>Add issue</h1>
 
       {/* title input */}
       <Text mb='8px'>Title</Text>
-      <Input mb='15px' variant='filled' placeholder='Title' />
+      <Input mb='15px' variant='filled' placeholder='Title' size='lg' />
       {/* description input */}
       <Text mb='8px'>Description</Text>
       <Textarea
@@ -24,13 +24,14 @@ export function CreateIssuePage() {
         mb='15px'
         variant='filled'
         placeholder='https://github.com/xyz/abc'
+        size='lg'
       />
-      {/* <div className='flex-r button-group'> */}
-      <Link to='/' className='btn btn-primary text-md btn-back'>
-        Go Back
-      </Link>
-      {/* <button className='btn btn-primary text-md'>Create Issue</button> */}
-      {/* </div> */}
-    </Container>
+      <div className='flex-r button-group'>
+        <Link to='/' className='btn btn-primary text-md btn-back'>
+          Go Back
+        </Link>
+        <button className='btn btn-primary text-md'>Create Issue</button>
+      </div>
+    </div>
   );
 }
