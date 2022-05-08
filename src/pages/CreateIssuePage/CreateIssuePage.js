a8e8069180aa28bf29ad4dd6358095a23eda2c0c
Ankit Kumain <kumankit10@gmail.com>
2022-05-08 15:16:42 +0530

    refactor: create issue page

--- src/pages/CreateIssuePage/CreateIssuePage.js
+++ src/pages/CreateIssuePage/CreateIssuePage.js
@@ -4,30 +4,29 @@ import { Heading, Input, Text, Container, Textarea } from "@chakra-ui/react";
 export function CreateIssuePage() {
   return (
     <div className='popup-container'>
-      <Container>
-        <Heading as='h2' size='xl'>
-          Add Issue
-        </Heading>
-        {/* title input */}
-        <Text mb='8px'>Title</Text>
-        <Input mb='15px' variant='filled' placeholder='Title' />
-        {/* description input */}
-        <Text mb='8px'>Description</Text>
-        <Textarea
-          mb='15px'
-          variant='filled'
-          placeholder='Enter description'
-          size='sm'
-        />
-        {/* description input */}
-        <Text mb='8px'>Repository URL</Text>
-        <Input
-          mb='15px'
-          variant='filled'
-          placeholder='https://github.com/xyz/abc'
-        />
-        <button className='btn btn-primary text-md'>Create Issue</button>
-      </Container>
+      <Heading as='h2' size='xl' p={8}>
+        Add Issue
+      </Heading>
+
+      {/* title input */}
+      <Text mb='8px'>Title</Text>
+      <Input mb='15px' variant='filled' placeholder='Title' />
+      {/* description input */}
+      <Text mb='8px'>Description</Text>
+      <Textarea
+        mb='15px'
+        variant='filled'
+        placeholder='Enter description'
+        size='sm'
+      />
+      {/* description input */}
+      <Text mb='8px'>Repository URL</Text>
+      <Input
+        mb='15px'
+        variant='filled'
+        placeholder='https://github.com/xyz/abc'
+      />
+      <button className='btn btn-primary text-md'>Create Issue</button>
     </div>
   );
 }
