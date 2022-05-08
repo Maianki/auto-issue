64cbcb7c085c6ce91e9d17ba1120046629743d3b
Ankit Kumain <kumankit10@gmail.com>
2022-05-08 14:40:16 +0530

    refctor: routing link

--- src/pages/CreateIssuePage/CreateIssuePage.js
+++ src/pages/CreateIssuePage/CreateIssuePage.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function CreateIssuePage() {
-  return <div className='popup-container'></div>;
+  return (
+    <div className='popup-container'>
+      <h1>This is create issue page</h1>
+    </div>
+  );
 }
