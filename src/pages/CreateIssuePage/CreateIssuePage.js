f6b046522e84ee145eb31e7a84db609f894d6194
Ankit Kumain <kumankit10@gmail.com>
2022-05-08 15:07:30 +0530

    feat: add create issue page

--- src/pages/CreateIssuePage/CreateIssuePage.js
+++ src/pages/CreateIssuePage/CreateIssuePage.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Heading, Input, Text, Container, Textarea } from "@chakra-ui/react";
 
 export function CreateIssuePage() {
   return (
     <div className='popup-container'>
-      <h1>This is create issue page</h1>
+      <Container>
+        <Heading as='h2' size='xl'>
+          Add Issue
+        </Heading>
+        {/* title input */}
+        <Text mb='8px'>Title</Text>
+        <Input mb='15px' variant='filled' placeholder='Title' />
+        {/* description input */}
+        <Text mb='8px'>Description</Text>
+        <Textarea
+          mb='15px'
+          variant='filled'
+          placeholder='Enter description'
+          size='sm'
+        />
+        {/* description input */}
+        <Text mb='8px'>Repository URL</Text>
+        <Input
+          mb='15px'
+          variant='filled'
+          placeholder='https://github.com/xyz/abc'
+        />
+        <button className='btn btn-primary text-md'>Create Issue</button>
+      </Container>
     </div>
   );
 }
